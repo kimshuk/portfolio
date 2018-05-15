337964b73b53e576ce9d13541297ecfbcd81a3db
developer <jeesoo2002@gmail.com>
2018-05-15 16:50:01 -0700

    main left div title and sub added

--- src/components/js/LeftDiv.js
+++ src/components/js/LeftDiv.js
@@ -4,10 +4,12 @@ import "../css/LeftDiv.css";
 const LeftDiv = () => {
   return (
     <section className="leftNav">
-      <a href="/" className="logo-link">
+      <a href="/" className="main-logo-link">
         <h1>Portfolio Website</h1>
       </a>
-      <p>I develop websites and web applications </p>
+      <p className="main-description">
+        I develop websites and web applications{" "}
+      </p>
     </section>
   );
 };
