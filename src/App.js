8952cf31211552afcce1b6aba322a1a4ad262916
developer <jeesoo2002@gmail.com>
2018-05-15 16:10:52 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+import LeftDiv from "./components/js/LeftDiv";
+
+class App extends Component {
+  render() {
+    return <LeftDiv />;
+  }
+}
+
+export default App;
---
+++ src/components/js/LeftDiv.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../css/LeftDiv.css";
+
+const LeftDiv = () => {
+  return (
+    <section className="leftNav">
+      <a href="/" className="logo-link">
+        <h1>Portfolio Website</h1>
+      </a>
+      <p>I develop websites and web applications </p>
+    </section>
+  );
+};
+
+export default LeftDiv;
