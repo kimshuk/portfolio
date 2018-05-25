4a74a5124fc8b53d9ba23b7a2430457ed9b06fba
developer <jeesoo2002@gmail.com>
2018-05-24 17:00:55 -0700

    update whatever i did so far

--- src/components/js/Project.js
+++ src/components/js/Project.js
@@ -2,16 +2,29 @@ import React, { Component } from "react";
 import "../css/Project.css";
 
 class Project extends Component {
+  static defaultProps = {
+    github: "Github",
+    liveLink: "Live Link"
+  };
+
   render() {
     console.log(this.props, " project");
     const { title, desc } = this.props;
 
     return (
       <div className="project">
-        <a>
-          <article>
-            <h2>{title}</h2>
-            <p>{desc}</p>
+        <a className="project__link">
+          <article className="project__info">
+            <span className="project__content">
+              <h2>{title}</h2>
+              <p>{desc}</p>
+              <a href="" className="project__btn">
+                {this.props.github}
+              </a>
+              <a href="" className="project__btn">
+                {this.props.liveLink}
+              </a>
+            </span>
           </article>
         </a>
       </div>
