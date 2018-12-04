63f7ff3f9faddbea88f9cd7b74c1c673e962255b
Andrew Kim <jeesoo2002@gmail.com>
2018-12-03 21:26:07 -0800

    update project

--- src/components/js/RightDiv.js
+++ src/components/js/RightDiv.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../css/RightDiv.css";
 import ProjectList from "./ProjectList";
+import advReactImg from "../img/advreact_preview.png";
 import metrolinkImg from "../img/metrolink_preview.png";
 import siebertImg from "../img/siebertnet_preview.png";
 import rejuvImg from "../img/rejuvenation_preview.png";
@@ -13,6 +14,14 @@ class RightDiv extends Component {
     projects: [
       {
         id: 0,
+        title: "Ecommerce with React",
+        desc: "React.js, GraphQL, Apollo",
+        imgPath: advReactImg,
+        ghLink: "https://github.com/kimshuk/Adv_react",
+        liveLink: "https://andrew-react-prod.herokuapp.com/"
+      },
+      {
+        id: 1,
         title: "Metrolink Trains",
         desc: "jQuery/JS, Vue.js, EpiServer, MVC, ASP.NET",
         imgPath: metrolinkImg,
@@ -20,7 +29,7 @@ class RightDiv extends Component {
         liveLink: "https://www.metrolinktrains.com/"
       },
       {
-        id: 1,
+        id: 2,
         title: "Siebertnet",
         desc: "jQuery/JS, Sitefinity, MVC, Webform, ASP.NET",
         imgPath: siebertImg,
@@ -28,15 +37,15 @@ class RightDiv extends Component {
         liveLink: "https://siebertnet.com/"
       },
       {
-        id: 2,
+        id: 3,
         title: "Rejuvenation Wellness",
-        desc: "Larvel, jQuery/JS",
+        desc: "Laravel, jQuery/JS",
         imgPath: rejuvImg,
         ghLink: "",
         liveLink: "http://rejuvenationwellness.org/"
       },
       {
-        id: 3,
+        id: 4,
         title: "OCTA Metrolink Weekends",
         desc: "jQuery/JS, ASP.NET",
         imgPath: octaImg,
@@ -45,7 +54,7 @@ class RightDiv extends Component {
           "http://www.octa.net/Metrolink/Metrolink-Weekends/Destinations/"
       },
       {
-        id: 4,
+        id: 5,
         title: "To do List",
         desc: "React.js",
         imgPath: todoImg,
@@ -53,7 +62,7 @@ class RightDiv extends Component {
         liveLink: "/todo-list"
       },
       {
-        id: 5,
+        id: 6,
         title: "Portfolio Website",
         desc: "React.js",
         imgPath: portfolioImg,
@@ -61,7 +70,7 @@ class RightDiv extends Component {
         liveLink: "http://jeesookim.com/"
       },
       {
-        id: 6,
+        id: 7,
         title: "Project Placeholder",
         desc: "About Project and blah blah",
         imgPath: "http://via.placeholder.com/300x500",
